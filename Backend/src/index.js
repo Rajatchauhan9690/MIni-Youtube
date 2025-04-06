a6f4c6bde3e2b871cc024a635665645de4ee722a
Rajat Chauhan <rajatchauhan9690@gmail.com>
2025-04-06 11:41:48 +0530

    Install Dependencies

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.get("/api/jokes", (req, res) => {
   const jokes = [
