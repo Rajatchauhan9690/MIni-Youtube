ab2a9529f5fbf1b9007c7e3e363720f28e382cb8
Rajat Chauhan <rajatchauhan9690@gmail.com>
2025-04-09 11:40:26 +0530

    Updated models and created utils for the errors and response

---
+++ Backend/src/utils/asynchandler.js
@@ -0,0 +1,17 @@
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message || "Internal Server Error",
+//     });
+//   }
+// };
+
+const asyncHandler = (fn) => {
+  return (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+  };
+};
+export default asyncHandler;
