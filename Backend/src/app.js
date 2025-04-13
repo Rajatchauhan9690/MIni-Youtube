f6b6babc0d0129f84dda925ab296c15ef00655db
Rajat Chauhan <rajatchauhan9690@gmail.com>
2025-04-13 10:22:00 +0530

    change in user controller and create user routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,4 +7,7 @@ app.use(express.json({ limit: "16 kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16 kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// Import routes
+import userRouter from "./routes/user.routes.js";
 export default app;
--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -3,3 +3,4 @@ import asyncHandler from "../utils/asynchandler.js";
 const registerUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "User registered" });
 });
+export { registerUser };
